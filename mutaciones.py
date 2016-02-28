a13a4ff28df4063b904364efe3447d16778866a0
ezequiel <ezequielbrrt@gmailcom>
2016-02-27 21:53:37 -0600

    Tipos de mutaciones

---
+++ mutaciones.py
@@ -0,0 +1,15 @@
+#! /bin/python
+# -*- coding: utf-8 -*-
+"""
+Tipos de mutación para el desarrollo de un algoritmo evolutivo
+
+
+"""
+
+def mutacion_uniforme():
+	pass
+
+def main():
+	print "Tipos de Mutacion"
+
+main()
