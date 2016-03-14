93fb5cf7e504ad81446bac46a96ca4b56b598fe1
Ezequiel Barreto <ezequielbrrt@gmail.com>
2016-03-13 21:56:28 -0600

    Update mutaciones.py

--- mutaciones.py
+++ mutaciones.py
@@ -1,15 +1,52 @@
-#! /bin/python
+#! /usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-Tipos de mutación para el desarrollo de un algoritmo evolutivo
 
+import numpy as np
+import random
 
-"""
-
-def mutacion_uniforme():
-	pass
+def mutacion_uniforme(poblacion,porcentajeMutacion):
+	pm = float(porcentajeMutacion)/100
+	nuevaPoblacion = []
+	for individuo in poblacion:
+		ind = list(individuo)
+		for i in range(len(ind)):
+			r = random.random()
+			if r > pm:
+				print "mutacion"
+				if ind[i] == "0":
+					ind[i] = "1" 
+				elif ind[i] == "1":
+					ind[i] = "0"
+		ind = "".join(ind)
+		nuevaPoblacion.append(ind)
+	print nuevaPoblacion
+	"""
+	for i in poblacion:
+		ind = list(i)
+		for j in range(len(ind)):
+			p = np.random.random_sample()
+			if p > 0.001 and p < 0.010:
+				print "mutacion"
+				if ind[j] == "0":
+					ind[j] = "1" 
+				elif ind[j] == "1":
+					ind[j] = "0"
+		ind = "".join(ind)
+		nuevaPoblacion.append(ind)
+	return nuevaPoblacion
+	"""
+	"""
+	for i in poblacion:
+		porcentaje = np.random.random_sample()	
+		if porcentaje > (float(porcentajeMutacion)/100):
+				print porcentaje	
+	"""
 
 def main():
-	print "Tipos de Mutacion"
+	poblacion = ["10101010","01010101","10000000","01010101","01010101"]
+	porcentajeMutacion = 90
+	print poblacion
+	mutacion_uniforme(poblacion,porcentajeMutacion)
+
 
 main()
